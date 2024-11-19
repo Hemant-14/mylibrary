47da0d00b0ce6b2c43ab798196919c5baa0c6c10
Hemant-14 <hemant14thakur14@gmail.com>
2024-11-19 14:19:43 +0530

    b

---
+++ vars/cicd1.groovy
@@ -0,0 +1,16 @@
+def download(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def build()
+{
+  sh 'mvn package'
+}
+def deploy(jobname,ip,context)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'
+}
+def test(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
